6804e5f391600fc3f284ce9fe6b80e8bde025510
Edgarilinet <krozeedgar@gmail.com>
2023-02-27 13:03:53 +0500

    .

--- public/main.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{let e=document.getElementById("disp"),t=document.getElementById("history");document.getElementById("disp-itog"),e.textContent="",t.textContent="Введите пример",new Array,new Array,new Array})();
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -36,7 +36,6 @@ module.exports = {
           //     }
           //   }
           // },
-          "sass-loader",
         ],
       },
     ],
